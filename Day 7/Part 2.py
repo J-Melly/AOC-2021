3da41cba75584e657c4df12d0463acdb0591a3a0
J-Melly <95376556+J-Melly@users.noreply.github.com>
2021-12-07 11:17:10 -0600

    Create Part 2.py

---
+++ Day 7/Part 2.py
@@ -0,0 +1,24 @@
+#Import List
+with open('Day7.txt') as file:
+  import_list = file.readlines()
+  import_list[0] = import_list[0].strip()
+
+data = import_list[0].split(',')
+crabs = [int(item) for item in data]
+
+#Function to determine fuel spent
+def fuel(my_list, pos):
+  sum = 0
+  for i in range(len(my_list)):
+    delta = lambda d : 0.5*d**2 + 0.5*d
+    sum += int(delta(abs(my_list[i] - pos)))
+
+  return sum  
+
+fuel_list = []
+
+for i in range(len(crabs)):
+  fuel_list.append(fuel(crabs,i))
+
+
+print(min(fuel_list))
