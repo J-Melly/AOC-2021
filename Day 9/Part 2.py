0a37f25a966151d5e534e59fbd2186032f74da9d
J-Melly <95376556+J-Melly@users.noreply.github.com>
2021-12-09 21:36:04 -0600

    Update Part 2.py

--- Day 9/Part 2.py
+++ Day 9/Part 2.py
@@ -14,20 +14,20 @@ def crawl(my_list, i, j):
   len_basin = len(basin)
  
   while counter < len_basin:
-    #Check left. 
     
+    #Check left. If the value is not a 9, append those coordinates to the current basin list.
     if int(my_list[basin[counter][0]][basin[counter][1]-1]) != 9 and basin[counter][1] != 0:
         new_list = [basin[counter][0],basin[counter][1]-1]
         if basin.count(new_list) == 0:
           basin.append(new_list)
 
-    #Check up
+    #Check up. If the value is not a 9, append those coordinates to the current basin list.
     if int(my_list[basin[counter][0]-1][basin[counter][1]]) != 9 and basin[counter][0] != 0:
         new_list = [basin[counter][0]-1,basin[counter][1]]
         if basin.count(new_list) == 0:
           basin.append(new_list)
     
-    #Check right
+    #Check right. If the value is not a 9, append those coordinates to the current basin list.
     try:
       if int(my_list[basin[counter][0]][basin[counter][1]+1]) != 9:
         new_list = [basin[counter][0],basin[counter][1]+1]
@@ -36,7 +36,7 @@ def crawl(my_list, i, j):
     except:
       pass
     
-    #Check down
+    #Check down. If the value is not a 9, append those coordinates to the current basin list.
     try:
       if int(my_list[basin[counter][0]+1][basin[counter][1]]) != 9:
         new_list = [basin[counter][0]+1,basin[counter][1]]
@@ -45,6 +45,7 @@ def crawl(my_list, i, j):
     except:
       pass
     
+    #Add 1 to the counter and check the new length of the basin.  The loop will continue until every coordinate in the basin list has be checked for adjacent coordinates in the basin
     counter += 1
     len_basin = len(basin)
   return len_basin
@@ -93,8 +94,10 @@ for i in range(len(my_list)):
 
 basin_sizes = []
 
+#Send each low point to the basin_sizes function to have the size calculated and returned
 for i in range(len(low_points)):
   basin_sizes.append(int(crawl(my_list, low_points[i][0], low_points[i][1])))
 
+#Sort the basin sizes highest to lowest and multiply the top three
 basin_sizes.sort(reverse=True)
 print(basin_sizes[0]*basin_sizes[1]*basin_sizes[2])
